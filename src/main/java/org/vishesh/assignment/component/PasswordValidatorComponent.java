1e3ac9b9dbdad781570714835583c93faf73127d
Vishesh <vishesh.happy@gmail.com>
2022-10-20 18:51:45 +0530

    Created a empty component

---
+++ src/main/java/org/vishesh/assignment/component/PasswordValidatorComponent.java
@@ -0,0 +1,11 @@
+package org.vishesh.assignment.component;
+
+import org.vishesh.assignment.manager.RulesManager;
+
+public class PasswordValidatorComponent {
+
+    public void validatePassword(final String password){
+        
+    }
+
+}
